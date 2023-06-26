a30a980284ff7cc0bed2a2a53d07f24717ecf8a2
hongyunsu <dbstn9@hanmail.net>
2023-06-26 21:45:22 +0900

    [🎉Init]: 프로젝트 초기 셋팅

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+function App() {
+	return (
+		<Container>
+			<div></div>
+		</Container>
+	);
+}
+
+export default App;
